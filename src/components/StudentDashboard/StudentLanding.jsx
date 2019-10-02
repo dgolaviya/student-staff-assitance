25cc99834270e320c112e381cada94aceb3d7bd3
Prateek Goel <goel.prateek27@gmail.com>
2019-10-02 10:45:21 +0530

    add static cards on student landing page

--- src/components/StudentDashboard/StudentLanding.jsx
+++ src/components/StudentDashboard/StudentLanding.jsx
@@ -1,8 +1,70 @@
 import React from 'react';
+import { Card, Row, Col, Icon } from 'react-materialize';
 
 const StudentLanding = () => {
   return (
-    <div>Student landing page</div>
+       <>
+    <Row style={{ padding: '0 25px' }}>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>account_circle</Icon>
+          <div className="card-header">30</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>android</Icon>
+          <div className="card-header">40</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>announcement</Icon>
+          <div className="card-header">10</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>assignment</Icon>
+          <div className="card-header">50</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+    </Row>
+    <Row style={{ padding: '0 25px' }}>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>assignment_turned_in</Icon>
+          <div className="card-header">20</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>build</Icon>
+          <div className="card-header">30</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>description</Icon>
+          <div className="card-header">30</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+      <Col m={3} s={6}>
+        <Card className="blue item-card lighten-2">
+          <Icon large>help</Icon>
+          <div className="card-header">30</div>
+          <div>This is simple card</div>
+        </Card>
+      </Col>
+    </Row>
+    </>
   );
 }
 

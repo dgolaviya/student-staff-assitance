5fdfab5e00a0f6fa02a63672a598cb79ecf6d6b8
Divyang Golaviya <pateldivu5@gmail.com>
2019-10-02 20:29:52 +0530

    show user name in left panel.

--- src/components/LeftMenu/LeftMenu.jsx
+++ src/components/LeftMenu/LeftMenu.jsx
@@ -28,23 +28,23 @@ class LeftMenu extends React.Component {
     this.setState({ selectedMenuItem: title });
   };
   render() {
-    const { menuItems, user, avatarDetail: { avatar, contentType }  } = this.props;
+    const { menuItems, userName, firstName, lastName, avatarDetail: { avatar, contentType } } = this.props;
     return (
-      <div className="left-menu col s4 m3 l2">
+      <div className="left-menu col s4 m6 l2">
         <div className="user-details">
           {avatar ? <img
+            className="avatar-image circle responsive-img"
+            alt=""
+            src={`data:${contentType};base64, ${avatar}`}
+          /> : <img
               className="avatar-image circle responsive-img"
               alt=""
-              src={`data:${contentType};base64, ${avatar}`}
-            /> : <img
-                className="avatar-image circle responsive-img"
-                alt=""
-                src={defaultAvatar}
-              />}
-          <span className="user-name center-align"><b>{user.name}</b></span>
+              src={defaultAvatar}
+            />}
+          <span className="user-name center-align"><b>{userName}</b></span>
           <div className="user-skills black-text">
-            <b>Full Stack Developer</b>
-            <b>Computer Engineer</b>
+            <b>{`${firstName} ${lastName}`}</b>
+            <b>{this.props.emailId}</b>
           </div>
         </div>
         <div>
@@ -67,7 +67,11 @@ class LeftMenu extends React.Component {
 }
 const mapStateToProps = (state) => ({
   avatarDetail: state.auth.avatarDetail,
-  userId: state.auth.user.userId
+  userId: state.auth.user.userId,
+  userName: state.auth.user.userName,
+  firstName: state.auth.user.firstName,
+  lastName: state.auth.user.lastName,
+  emailId: state.auth.user.emailId,
 });
 const mapDispatchToProps = (dispatch) => ({
   fetchAvatarImage: (userId) => dispatch(fetchAvatarImage(userId)),
